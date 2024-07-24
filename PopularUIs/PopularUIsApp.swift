d535d5e79dfbe88234ce0aafe944b8496c510134
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-24 14:24:36 +0200

    Initial Commit

---
+++ PopularUIs/PopularUIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularUIsApp.swift
+//  PopularUIs
+//
+//  Created by admin on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct PopularUIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
