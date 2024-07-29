9e23d1c81cd80fb9fe0c461ea7bec4e912d4e372
krowqa2k <105343696+krowqa2k@users.noreply.github.com>
2024-07-29 15:45:47 +0200

    feat
    created bumble chat preview cell

---
+++ PopularUIs/Bumble/Components/BumbleChatPreviewCell.swift
@@ -0,0 +1,55 @@
+//
+//  BumbleChatPreviewCell.swift
+//  PopularUIs
+//
+//  Created by admin on 29/07/2024.
+//
+
+import SwiftUI
+
+struct BumbleChatPreviewCell: View {
+    
+    var imageName: String = Constants.randomImage
+    var percentageRemaining: Double = Double.random(in: 0...1)
+    var hasNewMessage: Bool = true
+    var userName: String = "Mateusz"
+    var lastChatMessage: String? = "This is the last message."
+    var isYourMove: Bool = true
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            BumbleProfileImageCell(imageName: imageName, percentageRemaining: percentageRemaining, hasNewMessage: false)
+                .padding(.horizontal, 6)
+            
+            VStack(alignment: .leading, spacing: 0) {
+                HStack {
+                    Text(userName)
+                        .font(.title3)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    if isYourMove {
+                        Text("YOUR MOVE")
+                            .font(.caption)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                            .frame(width: 75)
+                            .padding(4)
+                            .padding(.horizontal, 2)
+                            .background(Color.bumbleYellow)
+                            .cornerRadius(12)
+                    }
+                }
+                
+                Text(lastChatMessage ?? "")
+                    .font(.subheadline)
+                    .foregroundStyle(Color.bumbleGray)
+            }
+            .lineLimit(1)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    BumbleChatPreviewCell()
+}
